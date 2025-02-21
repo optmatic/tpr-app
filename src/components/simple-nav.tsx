80e3c342277ff51043b244fa52a34ef4010746a2
thomas <tom@optmatic.com>
2025-02-21 10:33:12 +1000

    some fix

--- src/components/simple-nav.tsx
+++ src/components/simple-nav.tsx
@@ -8,13 +8,13 @@ import {
   type LucideIcon,
 } from "lucide-react"
 
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuSeparator,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu"
+// import {
+//   DropdownMenu,
+//   DropdownMenuContent,
+//   DropdownMenuItem,
+//   DropdownMenuSeparator,
+//   DropdownMenuTrigger,
+// } from "@/components/ui/dropdown-menu"
 import {
   SidebarGroup,
   SidebarGroupLabel,
